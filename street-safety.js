6886499fdd7949f0da799a45977ad93dc6c7fafc
Minji Koo <meaningfulokdol@gmail.com>
2023-07-13 07:38:22 +0900

    0713 8th commit

--- street-safety.js
+++ street-safety.js
@@ -1,10 +1,35 @@
-let star = document.querySelectorAll('input');
-let showValue = document.querySelector('#rating-value');
+let stars1 = document.querySelectorAll('input[name="rating_1"]');
+let showValue1 = document.querySelector('#rating-value_1');
 
-for (let i = 0; i < star.length; i++) {
-	star[i].addEventListener('click', function() {
-		i = this.value;
+for (let i = 0; i < stars1.length; i++) {
+  stars1[i].addEventListener('click', function() {
+    let rating = this.value;
+    let formattedRating = Number(rating).toFixed(1);
 
-		showValue.innerHTML = i;
-	});
+    showValue1.innerHTML = formattedRating;
+  });
+}
+
+let stars2 = document.querySelectorAll('input[name="rating_2"]');
+let showValue2 = document.querySelector('#rating-value_2');
+
+for (let i = 0; i < stars2.length; i++) {
+  stars2[i].addEventListener('click', function() {
+    let rating = this.value;
+    let formattedRating = Number(rating).toFixed(1);
+
+    showValue2.innerHTML = formattedRating;
+  });
+}
+
+let stars3 = document.querySelectorAll('input[name="rating_3"]');
+let showValue3 = document.querySelector('#rating-value_3');
+
+for (let i = 0; i < stars3.length; i++) {
+  stars3[i].addEventListener('click', function() {
+    let rating = this.value;
+    let formattedRating = Number(rating).toFixed(1);
+
+    showValue3.innerHTML = formattedRating;
+  });
 }
